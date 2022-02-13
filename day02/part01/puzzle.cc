cb3ea6f34a7aa8763e144af31bb215e42af9a403
Ove Svensson <ove.svensson@isv41.se>
2022-02-13 22:14:15 +0100

    day 2, part 1

---
+++ day02/part01/puzzle.cc
@@ -0,0 +1,11 @@
+
+#include "solver.hh"
+#include "../data.hh"
+#include <fmt/format.h>
+
+int main() {
+    const auto result = distance(data);
+    fmt::print("{}\n", result);
+}
+
+// answer: 1250395
---
+++ day02/solver/solver.hh
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "../data.hh"
+#include <cassert>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+unsigned distance(const std::vector<Course>& course)
+{
+    // unsigned fw{}, dn{};
+    // for (const auto& [directory, distance]: course) {}
+    const auto& [fw, dn] =
+        std::accumulate(std::begin(course), std::end(course), std::make_pair(0U, 0U),
+                        [](const auto& acc, const auto& dir) -> std::pair<unsigned, unsigned> {
+                            const auto& [forward, down] = acc;
+                            const auto& [direction, distance] = dir;
+                            switch (direction) {
+                            case Direction::FORWARD: return {forward + distance, down};
+                            case Direction::DOWN: return {forward, down + distance};
+                            case Direction::UP: return {forward, down - distance};
+                            }
+                            assert(false);
+                            return {};
+                        });
+    return fw * dn;
+}
