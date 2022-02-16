fda3c9e08220969219bad447d0abaf02b88d535d
Ove Svensson <ove.svensson@isv41.se>
2022-02-17 00:53:18 +0100

    day 4, part 1

--- day04/common/solver.hh
+++ day04/common/solver.hh
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "data.hh"
+
 #include <algorithm>
 #include <numeric>
 #include <optional>
 #include <tuple>
 
-bool check_bingo(const BingoBoard& board) {
+bool check_bingo(const BingoBoard& board)
+{
     auto check_row = [&b = board](std::size_t r) {
         return (b[r * 5 + 0] == -1) and (b[r * 5 + 1] == -1) and (b[r * 5 + 2] == -1) and
                (b[r * 5 + 3] == -1) and (b[r * 5 + 4] == -1);
@@ -19,13 +21,15 @@ bool check_bingo(const BingoBoard& board) {
            check_col(0) or check_col(1) or check_col(2) or check_col(3) or check_col(4);
 }
 
-unsigned calculate_score(const BingoBoard& board) {
+unsigned calculate_score(const BingoBoard& board)
+{
     return std::accumulate(std::begin(board), std::end(board), 0U, [](unsigned acc, int val) {
         return acc + ((val >= 0) ? static_cast<unsigned>(val) : 0U);
     });
 }
 
-BingoBoard mark_number(BingoBoard board, int number) {
+BingoBoard mark_number(BingoBoard board, int number)
+{
     const auto it = std::find(std::begin(board), std::end(board), number);
     if (it != std::end(board)) *it = -1;
     return board;
@@ -46,7 +50,7 @@ play_bingo(std::vector<BingoBoard>::iterator first_board,
     }
     return {};
 }
-}
+} // namespace details
 
 std::optional<std::tuple<BingoBoard, int>> play_bingo(std::vector<BingoBoard>& boards,
                                                       const std::vector<int>& numbers)
