e71687dff886e349291eae40ad27b47116838b89
Ove Svensson <ove.svensson@isv41.se>
2022-02-21 22:26:16 +0100

    day 7, part 2

--- day07/common/solver.hh
+++ day07/common/solver.hh
@@ -15,10 +15,9 @@ unsigned fuel_cost_1(const std::vector<int>& positions, int target) {
 }
 
 unsigned lowest_fuel_cost_1(const std::vector<int>& positions) {
-    const auto min = *std::min_element(std::begin(positions), std::end(positions));
-    const auto max = *std::max_element(std::begin(positions), std::end(positions));
-    std::vector<unsigned> possible_fuel_costs((max - min) + 1);
-    int target {min};
+    const auto [min, max] = std::minmax_element(std::begin(positions), std::end(positions));
+    std::vector<unsigned> possible_fuel_costs((*max - *min) + 1);
+    int target {*min};
     std::generate(std::begin(possible_fuel_costs),
                   std::end(possible_fuel_costs),
                   [&positions, &target]() { return fuel_cost_1(positions, target++); });
@@ -26,7 +25,7 @@ unsigned lowest_fuel_cost_1(const std::vector<int>& positions) {
 }
 
 unsigned fuel_cost_2(const std::vector<int>& positions, int target) {
-    // 0 + 1 + 2 + 3 + 4 + 5 + dist
+    // 0 + 1 + 2 + 3 + 4 + 5 + ... + dist
     const auto inc_fuel_const
       = [](unsigned dist) { return dist * ((dist + 1) / 2) + (dist / 2) * ((dist + 1) % 2); };
     const auto result = std::accumulate(std::begin(positions),
@@ -40,10 +39,9 @@ unsigned fuel_cost_2(const std::vector<int>& positions, int target) {
 }
 
 unsigned lowest_fuel_cost_2(const std::vector<int>& positions) {
-    const auto min = *std::min_element(std::begin(positions), std::end(positions));
-    const auto max = *std::max_element(std::begin(positions), std::end(positions));
-    std::vector<unsigned> possible_fuel_costs((max - min) + 1);
-    int target {min};
+    const auto [min, max] = std::minmax_element(std::begin(positions), std::end(positions));
+    std::vector<unsigned> possible_fuel_costs((*max - *min) + 1);
+    int target {*min};
     std::generate(std::begin(possible_fuel_costs),
                   std::end(possible_fuel_costs),
                   [&positions, &target]() { return fuel_cost_2(positions, target++); });
